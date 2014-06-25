6b47b2f409cb29f0a86cc107cf13776ba4b91632
Mickey Kay <mickey@mickeykaycreative.com>
2014-06-25 16:29:09 -0700

    phptidy on better-font-awesome.php.

--- better-font-awesome.php
+++ better-font-awesome.php
@@ -12,35 +12,35 @@
  */
 
 
-require_once( plugin_dir_path( __FILE__ ) . 'lib/better-font-awesome-library/better-font-awesome-library.php' );
+require_once plugin_dir_path( __FILE__ ) . 'lib/better-font-awesome-library/better-font-awesome-library.php';
 
 /**
  * Load Titan Framework
  */
- 
+
 // Don't do anything when we're activating a plugin to prevent errors
 // on redeclaring Titan classes
 if ( ! empty( $_GET['action'] ) && ! empty( $_GET['plugin'] ) ) {
-    if ( $_GET['action'] == 'activate' ) {
-        return;
-    }
+	if ( $_GET['action'] == 'activate' ) {
+		return;
+	}
 }
 // Check if the framework plugin is activated
 $useEmbeddedFramework = true;
-$activePlugins = get_option('active_plugins');
+$activePlugins = get_option( 'active_plugins' );
 if ( is_array( $activePlugins ) ) {
-    foreach ( $activePlugins as $plugin ) {
-        if ( is_string( $plugin ) ) {
-            if ( stripos( $plugin, 'lib/Titan-Framework.php' ) !== false ) {
-                $useEmbeddedFramework = false;
-                break;
-            }
-        }
-    }
+	foreach ( $activePlugins as $plugin ) {
+		if ( is_string( $plugin ) ) {
+			if ( stripos( $plugin, 'lib/Titan-Framework.php' ) !== false ) {
+				$useEmbeddedFramework = false;
+				break;
+			}
+		}
+	}
 }
 // Use the embedded Titan Framework
 if ( $useEmbeddedFramework && ! class_exists( 'TitanFramework' ) ) {
-    require_once( plugin_dir_path( __FILE__ ) . 'lib/Titan-Framework/titan-framework.php' );
+	require_once plugin_dir_path( __FILE__ ) . 'lib/Titan-Framework/titan-framework.php';
 }
 
 
@@ -81,7 +81,6 @@ class Better_Font_Awesome_Plugin {
 	 * Constructor
 	 */
 	function __construct() {
-
 		// Setup Titan instance
 		$this->titan = TitanFramework::getInstance( 'better-font-awesome' );
 
@@ -115,8 +114,8 @@ class Better_Font_Awesome_Plugin {
 		$jsdelivr_fetcher = new jsDeliver_Fetcher();
 
 		// jsDelivr CDN data
-		$this->jsdelivr_data['versions'] = $jsdelivr_fetcher->get_value('versions');
-		$this->jsdelivr_data['last_version'] = $jsdelivr_fetcher->get_value('lastversion');
+		$this->jsdelivr_data['versions'] = $jsdelivr_fetcher->get_value( 'versions' );
+		$this->jsdelivr_data['last_version'] = $jsdelivr_fetcher->get_value( 'lastversion' );
 	}
 
 	/**
@@ -126,46 +125,46 @@ class Better_Font_Awesome_Plugin {
 		// Setup available versions
 		$versions[ 'latest' ] = __( 'Always Latest', 'better-font-awesome' ) . ' (' . $this->jsdelivr_data['last_version'] . ')';
 
-		foreach( $this->jsdelivr_data['versions'] as $version ) {
+		foreach ( $this->jsdelivr_data['versions'] as $version ) {
 			// Exclude v2.0
 			if ( '2' != substr( $version, 0, 1 ) )
 				$versions[$version] = $version;
 		}
 
 		$optionsPage = $this->titan->createAdminPanel( array(
-		    'name' => __( 'Better Font Awesome', 'better-font-awesome'),
-		    'parent' => 'options-general.php',
-		) );
+				'name' => __( 'Better Font Awesome', 'better-font-awesome' ),
+				'parent' => 'options-general.php',
+			) );
 
 		$optionsPage->createOption( array(
-		    'name' => __( 'Font Awesome version', 'better-font-awesome' ),
-		    'id' => 'version',
-		    'type' => 'select',
-		    'desc' => __( 'Select the version of Font Awesome you would like to use. Visit the <a href="http://fontawesome.io/" target="_blank">Font Awesome website</a> for more information.', 'better-font-awesome') ,
-		    'options' => $versions,
-		    'default' => $this->jsdelivr_data['last_version'],
-		) );
+				'name' => __( 'Font Awesome version', 'better-font-awesome' ),
+				'id' => 'version',
+				'type' => 'select',
+				'desc' => __( 'Select the version of Font Awesome you would like to use. Visit the <a href="http://fontawesome.io/" target="_blank">Font Awesome website</a> for more information.', 'better-font-awesome' ) ,
+				'options' => $versions,
+				'default' => $this->jsdelivr_data['last_version'],
+			) );
 
 		$optionsPage->createOption( array(
-		    'name' => __( 'Use minified CSS', 'better-font-awesome' ),
-		    'id' => 'minified',
-		    'type' => 'checkbox',
-		    'desc' => __( 'Whether to include the minified version of the CSS (checked), or the unminified version (unchecked).', 'better-font-awesome' ),
-		    'default' => true,
-		) );
+				'name' => __( 'Use minified CSS', 'better-font-awesome' ),
+				'id' => 'minified',
+				'type' => 'checkbox',
+				'desc' => __( 'Whether to include the minified version of the CSS (checked), or the unminified version (unchecked).', 'better-font-awesome' ),
+				'default' => true,
+			) );
 
 		$optionsPage->createOption( array(
-		    'name' => __( 'Remove existing FA', 'better-font-awesome' ),
-		    'id' => 'remove_existing_fa',
-		    'type' => 'checkbox',
-		    'desc' => __( 'Remove Font Awesome CSS and shortcodes added by other plugins and themes. This may help if icons are not rendering properly.', 'better-font-awesome' ),
-		    'default' => false,
-		) );
+				'name' => __( 'Remove existing FA', 'better-font-awesome' ),
+				'id' => 'remove_existing_fa',
+				'type' => 'checkbox',
+				'desc' => __( 'Remove Font Awesome CSS and shortcodes added by other plugins and themes. This may help if icons are not rendering properly.', 'better-font-awesome' ),
+				'default' => false,
+			) );
 
 		$optionsPage->createOption( array(
-			'name' => __( 'Usage', 'better-font-awesome' ),
-		    'type' => 'note',
-		    'desc' => __( '
+				'name' => __( 'Usage', 'better-font-awesome' ),
+				'type' => 'note',
+				'desc' => __( '
 		    		<b>Version 4</b>&nbsp;&nbsp;&nbsp;<small><a href="http://fontawesome.io/examples/">See all available classes &raquo;</a></small><br /><br />
 		    		<i class="icon-star fa fa-star"></i> <code>[icon name="star"]</code> or <code>&lt;i class="fa-star"&gt;&lt;/i&gt;</code><br /><br />
 		    		<i class="icon-star fa fa-star icon-2x fa-2x"></i> <code>[icon name="star" class="fa-2x"]</code> or <code>&lt;i class="fa-star fa-2x"&gt;&lt;/i&gt;</code><br /><br />
@@ -176,11 +175,11 @@ class Better_Font_Awesome_Plugin {
 		    		<i class="icon-star fa fa-star icon-2x fa-2x icon-border fa-border"></i> <code>[icon name="star" class="icon-2x icon-border"]</code> or <code>&lt;i class="icon-star icon-2x icon-border"&gt;&lt;/i&gt;</code>
 
 		    		', 'better-font-awesome' ),
-		) );
+			) );
 
 		$optionsPage->createOption( array(
-		    'type' => 'save',
-		) );
+				'type' => 'save',
+			) );
 	}
 
 	function do_better_font_awesome_library() {
