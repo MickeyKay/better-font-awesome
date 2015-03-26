f974bf693db1f90ffbd6d00acd3a0ba9672c8696
dashaluna <dashaluna@gmail.com>
2015-03-26 14:05:59 +0000

    Use output buffer to long and complex HTML snippets

--- better-font-awesome.php
+++ better-font-awesome.php
@@ -212,40 +212,42 @@ class Better_Font_Awesome_Plugin {
     public function deactivate() {
         
         deactivate_plugins( plugin_basename( __FILE__ ) );
+	    ob_start();
+	    ?>
 
-        $message = sprintf(
-	        '<h2>%s</h2>',
-	        esc_html__( 'Better Font Awesome', 'better-font-awesome' )
-        );
+	    <h2><?php esc_html_e( 'Better Font Awesome', 'better-font-awesome' ); ?></h2>
 
-	    $message .= '<p>' .
-	                sprintf(
-		                esc_html__(
-			                'It appears that Better Font Awesome is missing it\'s %1$score library%5$s, '.
-			                'which typically occurs when cloning the Git repository and not updating all ' .
-			                'submodules. Please refer to the plugin\'s %2$sinstallation instructions%5$s ' .
-			                'for details on how to properly install Better Font Awesome via Git. If you ' .
-			                'installed from within WordPress, or via the wordpress.org repo, then chances ' .
-			                'are the install failed and you can try again. If the issue persists, please ' .
-			                'create a new topic on the plugin\'s %3$ssupport forum%5$s or file an issue ' .
-			                'on the %4$sGithub repo%5$s.',
-			                'better-font-awesome'
-		                ),
-		                '<a href="https://github.com/MickeyKay/better-font-awesome-library" target="_blank">',
-		                '<a href="https://github.com/MickeyKay/better-font-awesome" target="_blank">',
-		                '<a href="http://wordpress.org/support/plugin/better-font-awesome" target="_blank">',
-		                '<a href="https://github.com/MickeyKay/better-font-awesome/issues" target="_blank">',
-		                '</a>'
-	                ) .
-	                '</p>';
+	    <p>
+		    <?php
+		    printf(
+			    esc_html__(
+				    'It appears that Better Font Awesome is missing it\'s %1$score library%5$s, '.
+				    'which typically occurs when cloning the Git repository and not updating all ' .
+				    'submodules. Please refer to the plugin\'s %2$sinstallation instructions%5$s ' .
+				    'for details on how to properly install Better Font Awesome via Git. If you ' .
+				    'installed from within WordPress, or via the wordpress.org repo, then chances ' .
+				    'are the install failed and you can try again. If the issue persists, please ' .
+				    'create a new topic on the plugin\'s %3$ssupport forum%5$s or file an issue ' .
+				    'on the %4$sGithub repo%5$s.',
+				    'better-font-awesome'
+			    ),
+			    '<a href="https://github.com/MickeyKay/better-font-awesome-library" target="_blank">',
+			    '<a href="https://github.com/MickeyKay/better-font-awesome" target="_blank">',
+			    '<a href="http://wordpress.org/support/plugin/better-font-awesome" target="_blank">',
+			    '<a href="https://github.com/MickeyKay/better-font-awesome/issues" target="_blank">',
+			    '</a>'
+		    );
+		    ?>
+	    </p>
 
-	    $message .= sprintf(
-		    '<p><a href="%s">%s</a></p>',
-		    esc_url( get_admin_url( null, 'plugins.php' ) ),
-		    esc_html__( 'Back to the plugins page &rarr;', 'better-font-awesome' )
-	    );
+	    <p>
+		    <a href="<?php echo esc_url( get_admin_url( null, 'plugins.php' ) ); ?>">
+			    <?php esc_html_e( 'Back to the plugins page &rarr;', 'better-font-awesome' ); ?>
+		    </a>
+	    </p>
 
-	    wp_die( $message );
+	    <?php
+	    wp_die( ob_get_clean() );
 
     }
 
@@ -489,15 +491,15 @@ class Better_Font_Awesome_Plugin {
             <p>
                 <?php 
                 printf( esc_html__( 'Font Awesome will still render using version: %s', 'better-font-awesome' ),
-                    '<code>' . $this->bfa_lib->get_fallback_version() . '</code>'
+	                '<code>' . $this->bfa_lib->get_fallback_version() . '</code>'
                 );
                 ?>
             </p>
             <p>
                 <?php
                 printf( esc_html__( 'This may be the result of a temporary server or connectivity issue which will resolve shortly. However if the problem persists please file a support ticket on the %splugin forum%s, citing the errors listed above. ', 'better-font-awesome' ),
-                        '<a href="http://wordpress.org/support/plugin/better-font-awesome" target="_blank" title="Better Font Awesome support forum">',
-                        '</a>'
+	                '<a href="http://wordpress.org/support/plugin/better-font-awesome" target="_blank" title="Better Font Awesome support forum">',
+	                '</a>'
                 );
                 ?>
             </p>
@@ -541,52 +543,67 @@ class Better_Font_Awesome_Plugin {
      * @return  string  Usage text.
      */
     public function get_usage_text() {
-        return '<div class="bfa-usage-text">' .
 
-                    '<h3>' . esc_html__( 'Usage', 'better-font-awesome' ) . '</h3>' .
-                    sprintf(
-	                    esc_html__(
-		                    'Font Awesome version 4.x + %s See all available options %s',
-		                    'better-font-awesome'
-	                    ),
-	                    '&nbsp;&nbsp;&nbsp;<small><a href="http://fontawesome.io/examples/">',
-	                    '&raquo;</a></small><br/><br/>'
-                    ) .
-                    sprintf(
-	                    '<i class="icon-%$1s fa fa-%$1s"></i> <code>[icon name="%$1s"]</code> %$2s <code>&lt;i class="fa-%$1s"&gt;&lt;/i&gt;</code><br /><br />' .
-	                    '<i class="icon-%$1s fa fa-%$1s icon-2x fa-2x"></i> <code>[icon name="%$1s" class="fa-2x"]</code> %$2s <code>&lt;i class="fa-%$1s fa-2x"&gt;&lt;/i&gt;</code><br /><br />' .
-	                    '<i class="icon-%$1s fa fa-%$1s icon-2x fa-2x icon-rotate-90 fa-rotate-90"></i> <code>[icon name="%$1s" class="fa-2x fa-rotate-90"]</code> %$2s <code>&lt;i class="fa-%$1s fa-2x fa-rotate-90"&gt;&lt;/i&gt;</code><br /><br /><br />',
-	                    esc_html__(
-		                    'coffee',
-		                    'better-font-awesome'
-	                    ),
-	                    esc_html__(
-		                    'or',
-		                    'better-font-awesome'
-	                    )
-                    ) .
-                    sprintf(
-	                    esc_html__(
-		                    'Font Awesome version 3.x %s See all available options %s',
-		                    'better-font-awesome'
-	                    ),
-	                    '&nbsp;&nbsp;&nbsp;<small><a href="http://fontawesome.io/3.2.1/examples/">',
-	                    '&raquo;</a></small><br/><br/>'
-                    ) .
-                    sprintf(
-	                    '<i class="icon-%$1s fa fa-%$1s"></i> <code>[icon name="%$1s"]</code> %$2s <code>&lt;i class="icon-%$1s"&gt;&lt;/i&gt;</code><br /><br />' .
-	                    '<i class="icon-%$1s fa fa-%$1s icon-2x fa-2x"></i> <code>[icon name="%$1s" class="icon-2x"]</code> %$2s <code>&lt;i class="icon-%$1s icon-2x"&gt;&lt;/i&gt;</code><br /><br />' .
-	                    '<i class="icon-%$1s fa fa-%$1s icon-2x fa-2x icon-rotate-90 fa-rotate-90"></i> <code>[icon name="%$1s" class="icon-2x icon-rotate-90"]</code> %$2s <code>&lt;i class="icon-%$1s icon-2x icon-rotate-90"&gt;&lt;/i&gt;</code>',
-	                    esc_html__(
-		                    'coffee',
-		                    'better-font-awesome'
-	                    ),
-	                    esc_html__(
-		                    'or',
-		                    'better-font-awesome'
-	                    )
-                    ) .
-               '</div>';
+	    ob_start();
+	    ?>
+
+	    <div class="bfa-usage-text">
+			<h3><?php esc_html_e( 'Usage', 'better-font-awesome' ); ?></h3>
+
+		    <b><?php printf( esc_html_x( 'Font Awesome version %s', 'For version 4.x +', 'better-font-awesome' ), '4.x +' ); ?></b>
+		    <small>
+			    <a href="http://fontawesome.io/examples/">
+				    <?php echo esc_html_x( 'See all available options', 'For version 4.x +', 'better-font-awesome' ); ?> &raquo;
+			    </a>
+		    </small>
+		    <br/><br/>
+
+		    <?php
+		    $code_alternative_str = esc_html_x( 'or', 'Text between two variations of code markup examples', 'better-font-awesome' );
+		    //TODO: should I allow these example codes to be translated or not? Add a WP filter for them instead?
+		    ?>
+
+		    <i class="icon-coffee fa fa-coffee"></i> <code>[icon name="coffee"]</code>
+		    <?php echo $code_alternative_str; ?>
+		    <code>&lt;i class="fa-coffee"&gt;&lt;/i&gt;</code>
+		    <br/><br/>
+
+		    <i class="icon-coffee fa fa-coffee icon-2x fa-2x"></i> <code>[icon name="coffee" class="fa-2x"]</code>
+		    <?php echo $code_alternative_str; ?>
+		    <code>&lt;i class="fa-coffee fa-2x"&gt;&lt;/i&gt;</code>
+		    <br/><br/>
+
+		    <i class="icon-coffee fa fa-coffee icon-2x fa-2x icon-rotate-90 fa-rotate-90"></i> <code>[icon name="coffee" class="fa-2x fa-rotate-90"]</code>
+		    <?php echo $code_alternative_str; ?>
+		    <code>&lt;i class="fa-coffee fa-2x fa-rotate-90"&gt;&lt;/i&gt;</code>
+		    <br/><br/><br/>
+
+		    <b><?php printf( esc_html_x( 'Font Awesome version %s', 'For version 3.x', 'better-font-awesome' ), '3.x' ); ?></b>
+		    <small>
+			    <a href="http://fontawesome.io/3.2.1/examples/">
+				    <?php echo esc_html_x( 'See all available options', 'For version 3.x', 'better-font-awesome' ); ?> &raquo;
+			    </a>
+		    </small>
+		    <br/><br/>
+
+		    <i class="icon-coffee fa fa-coffee"></i> <code>[icon name="coffee"]</code>
+		    <?php echo $code_alternative_str; ?>
+		    <code>&lt;i class="icon-coffee"&gt;&lt;/i&gt;</code>
+		    <br/><br/>
+
+		    <i class="icon-coffee fa fa-coffee icon-2x fa-2x"></i> <code>[icon name="coffee" class="icon-2x"]</code>
+		    <?php echo $code_alternative_str; ?>
+		    <code>&lt;i class="icon-coffee icon-2x"&gt;&lt;/i&gt;</code>
+		    <br/><br/>
+
+		    <i class="icon-coffee fa fa-coffee icon-2x fa-2x icon-rotate-90 fa-rotate-90"></i> <code>[icon name="coffee" class="icon-2x icon-rotate-90"]</code>
+		    <?php echo $code_alternative_str; ?>
+		    <code>&lt;i class="icon-coffee icon-2x icon-rotate-90"&gt;&lt;/i&gt;</code>
+
+	    </div>
+
+	    <?php
+	    return ob_get_clean();
     }
 
     /**
