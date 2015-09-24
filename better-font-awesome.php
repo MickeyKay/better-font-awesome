5d183e2be4908cec099592089b347ce353b631d8
MickeyKay <mickeyskay@gmail.com>
2015-09-24 12:25:10 -0700

    Fix: icon shortcode inserter not working in ACF Flexible fields.

--- better-font-awesome.php
+++ better-font-awesome.php
@@ -12,7 +12,7 @@
  * Plugin Name:       Better Font Awesome
  * Plugin URI:        http://wordpress.org/plugins/better-font-awesome
  * Description:       The ultimate Font Awesome icon plugin for WordPress.
- * Version:           1.4.1
+ * Version:           1.4.2
  * Author:            MIGHTYminnow & Mickey Kay
  * Author URI:        mickey@mickeykaycreative.com
  * License:           GPLv2+
