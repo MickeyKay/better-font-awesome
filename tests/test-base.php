e67efbf40d0e8303ccf6bf4d64e6b9baf3737ab8
MickeyKay <mickeyskay@gmail.com>
2017-06-04 21:38:27 -0700

    Alias unit test class

--- tests/test-base.php
+++ tests/test-base.php
@@ -1,5 +1,9 @@
 <?php
 
+// Support PHP 7+.
+// @see  https://stackoverflow.com/questions/42811164/class-phpunit-framework-testcase-not-found
+class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+
 class Test_Base extends PHPUnit_Framework_TestCase {
 
 	public function testOnePlusOne() {
