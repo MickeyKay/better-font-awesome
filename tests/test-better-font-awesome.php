9e8b09c87f6107566368b8cdad57d1f2f04e187e
MickeyKay <mickeyskay@gmail.com>
2020-09-25 19:51:07 -0700

    Fix tests

--- tests/test-better-font-awesome.php
+++ tests/test-better-font-awesome.php
@@ -1,10 +1,5 @@
 <?php
 
-// Support PHP 7+.
-// @see  https://stackoverflow.com/questions/42811164/class-phpunit-framework-testcase-not-found
-class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
-
-
 class Better_Font_Awesome_Test extends WP_UnitTestCase {
 
 	protected $bfa;
