b776cd94852e926bcb2662a3a53128818a76f6f3
Mickey Kay <mickeyskay@gmail.com>
2023-01-15 10:04:44 -0800

    2.0.4 updates (#47)

--- tests/test-better-font-awesome.php
+++ tests/test-better-font-awesome.php
@@ -5,6 +5,8 @@ class Better_Font_Awesome_Test extends WP_UnitTestCase {
 	protected $bfa;
 	protected $bfa_lib;
 
+	protected $core_font_awesome_version = '5.15.4';
+
 	public function setUp() {
         $this->bfa = Better_Font_Awesome_Plugin::get_instance( [] );
         $this->bfa_lib = $this->bfa->get_bfa_lib_instance( [] );
@@ -37,15 +39,15 @@ class Better_Font_Awesome_Test extends WP_UnitTestCase {
   	 */
 
   	public function test_bfal_version() {
-		$this->assertEquals( '5.15.2', $this->bfa_lib->get_version() );
+		$this->assertEquals( $this->core_font_awesome_version, $this->bfa_lib->get_version() );
   	}
 
   	public function test_get_stylesheet_url() {
-  		$this->assertEquals( 'https://use.fontawesome.com/releases/v5.15.2/css/all.css', $this->bfa_lib->get_stylesheet_url() );
+  		$this->assertEquals( 'https://use.fontawesome.com/releases/v' . $this->core_font_awesome_version . '/css/all.css', $this->bfa_lib->get_stylesheet_url() );
   	}
 
   	public function test_get_stylesheet_url_v4_shim() {
-  		$this->assertEquals( 'https://use.fontawesome.com/releases/v5.15.2/css/v4-shims.css', $this->bfa_lib->get_stylesheet_url_v4_shim() );
+  		$this->assertEquals( 'https://use.fontawesome.com/releases/v' . $this->core_font_awesome_version . '/css/v4-shims.css', $this->bfa_lib->get_stylesheet_url_v4_shim() );
   	}
 
   	public function test_get_icons() {
@@ -131,7 +133,16 @@ class Better_Font_Awesome_Test extends WP_UnitTestCase {
   					'class' => 'icon-rotate fa-2x',
   				],
   				'output' => '<i class="fa fa-bicycle fa-rotate fa-2x " ></i>',
-  			]
+  			],
+  			// Properly escapes dangerous input.
+  			[
+  				'atts' => [
+  					'name'  => '"< hack-name',
+  					'class' => '"< hack-class',
+  					'title' => '"< hack-title',
+  				],
+  				'output' => '<i class="fa fa-&quot;&lt; hack-name fa-&quot;&lt; fa-hack-class " title="&quot;&lt; hack-title" ></i>',
+  			],
   		];
 
   		foreach ( $shortcodes as $shortcode ) {
