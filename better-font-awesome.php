600495ef7ffa02d439f5fa35b5fa601c1731d059
MickeyKay <mickeyskay@gmail.com>
2026-09-07 11:19:46 -0700

    Group read-only settings information before checkboxes

--- better-font-awesome.php
+++ better-font-awesome.php
@@ -546,20 +546,20 @@ class Better_Font_Awesome_Plugin {
 		);
 
 		add_settings_field(
+			'version_check_frequency', // ID.
+			__( 'Version check frequency', 'better-font-awesome' ), // Title.
+			array( $this, 'version_check_frequency_callback' ), // Callback.
+			self::SLUG, // Page.
+			'settings_section_primary' // Section.
+		);
+
+		add_settings_field(
 			'asset_delivery',
 			__( 'Serve Font Awesome locally', 'better-font-awesome' ),
 			array( $this, 'asset_delivery_callback' ),
 			self::SLUG,
 			'settings_section_primary',
 			array( 'label_for' => 'asset_delivery' )
-		);
-
-		add_settings_field(
-			'version_check_frequency', // ID.
-			__( 'Version check frequency', 'better-font-awesome' ), // Title.
-			array( $this, 'version_check_frequency_callback' ), // Callback.
-			self::SLUG, // Page.
-			'settings_section_primary' // Section.
 		);
 
 		add_settings_field(
